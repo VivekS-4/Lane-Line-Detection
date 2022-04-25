c918a031425ba3454795c5ab05774f1bef1c0689
Vivek Sharma <vivshrmav4@gmail.com>
2022-04-25 12:04:34 +0530

    Update

--- lanes.py
+++ lanes.py
@@ -14,18 +14,21 @@ def canny(lane_image):
 def area_of_interest(image):
     height = image.shape[0]
     polygons = np.array([
-        [(200, height), (600, height), (265, 160)] #3rd point is in (x,y)
+        [(240, height), (600, height), (265, 165)] #3rd point is in (x,y)
         ])
     mask = np.zeros_like(image)
-    c.fillPoly(mask, polygons, 255)
-    return mask
+    c.fillPoly(mask, polygons, (255, 255, 255))
+    masked_image = c.bitwise_and(image, mask)
+    return masked_image
 
 img = c.imread('rd.jpg')
 lane_img = np.copy(img)
 
 # c.imshow('Result', canny(lane_img)) #displays the image
 # c.waitKey(0) # Delays the closing of cv2.imshow(), by specifying 0, it means till any other key is pressed from the keyboard
-plt.imshow(canny(lane_img))
+# plt.imshow(canny(lane_img))
+canny = canny(lane_img)
 plt.show() #it's working is same as cv2.waitkey()
 plt.imshow(area_of_interest(lane_img))
+cropped_image = area_of_interest(canny)
 plt.show() #it's working is same as cv2.waitkey()
